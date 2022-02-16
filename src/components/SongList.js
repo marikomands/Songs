8efa8ad0ebbd2b3c92c5ad20c5dd5675533652d3
marikomands <marikomands@gmail.com>
2022-02-16 11:20:14 +0000

    mapped this.props.songs

--- src/components/SongList.js
+++ src/components/SongList.js
@@ -2,16 +2,28 @@ import React, { Component } from "react";
 import { connect } from "react-redux";
 
 class SongList extends Component {
+  renderList() {
+    return this.props.songs.map((song) => {
+      return (
+        <div className="item" key={song.title}>
+          <div className="right floated content">
+            <button className="ui button primary">Select</button>
+          </div>
+          <div className="content">{song.title}</div>
+        </div>
+      );
+    });
+  }
   render() {
-    return <div>Songlist</div>;
+    return <div className="ui divided list">{this.renderList()}</div>;
   }
 }
 
-const mapStateToPop = (state) => {
+const mapStateToProps = (state) => {
   // state is reduers:  song and selectedSong
   console.log(state);
 
   return { songs: state.songs };
 };
 
-export default connect(mapStateToPop)(SongList);
+export default connect(mapStateToProps)(SongList);
